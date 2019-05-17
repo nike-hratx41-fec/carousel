a7b47b995f958f1c2faa8b626f07630b4f46accc
DAVID GUENTHER <davidguenther@DAVIDs-MacBook-Pro.local>
2019-05-17 10:32:05 -0500

    pushing up footer skeleton and carousel component

---
+++ src/components/Carousel.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Footer from './Footer'
+
+class Carousel extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+
+        // bind functions below
+    }
+
+    render() {
+        return (
+            <div>
+                <div>Carousel</div>
+                <div><Footer /></div>
+            </div>
+        )
+    }
+
+}
+
+export default Carousel;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Carousel from './components/Carousel';
+
+ReactDOM.render(<Carousel />, document.getElementById('carousel'));
