ad5346240128aa6f95c3149474ecfd92cff23d01
DAVID GUENTHER <davidguenther@DAVIDs-MacBook-Pro.local>
2019-05-18 15:41:47 -0500

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+        name: 'carousel',
+        script: './server/index.js'
+    }],
+    deploy: {
+        production: {
+            user: 'ubuntu',
+            host: 'ec2-18-220-230-181.us-east-2.compute.amazonaws.com',
+            key: '~/.ssh/fike-carousel.pem',
+            ref: 'origin/master',
+            repo: 'https://github.com/nike-hratx41-fec/carousel.git',
+            path: '/home/ubuntu/carousel',
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        }
+    }
+}
