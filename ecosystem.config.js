63516aac579785d85afec551d0f37630ca4dd2d8
DAVID GUENTHER <davidguenther@DAVIDs-MacBook-Pro.local>
2019-05-18 17:13:21 -0500

    added key

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
             host: 'ec2-18-220-230-181.us-east-2.compute.amazonaws.com',
             key: '~/.ssh/fike-carousel.pem',
             ref: 'origin/master',
-            repo: 'https://github.com/nike-hratx41-fec/carousel.git',
+            repo: 'git@github.com:nike-hratx41-fec/carousel.git',
             path: '/home/ubuntu/carousel',
             'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
         }
