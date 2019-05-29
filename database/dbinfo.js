8773c3d701ea6737d5c8a9942d2ca83d6b060508
DAVID GUENTHER <davidguenther@DAVIDs-MacBook-Pro.local>
2019-05-29 14:50:55 -0500

    added jquery for bootstrap

--- database/dbinfo.js
+++ database/dbinfo.js
@@ -1,6 +1,4 @@
 const username = `Dcguenther`;
 const password = `Summer@08`;
-const awsAccess = `AKIAWKK2QFEV3MSWCL2W`;
-const awsSecret = `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`
 
 module.exports = { username, password }
